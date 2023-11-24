dddc69f00018d0e354c344943e9ae166882f996b
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2023-11-24 18:07:47 +0800

    .

--- user/function/immunization.save.php
+++ user/function/immunization.save.php
@@ -4,12 +4,14 @@ include('../../function/db.php');
 $record_id = $_POST['record_id'];
 $patient_id = $_POST['patient_id'];
 
-// Define an array of input names
+// Define an array of input names, including the weight fields
 $inputNames = array(
-    'bcg_date_1', 'bcg_date_2', 'pentavalent_date_1', 'pentavalent_date_2', 'pentavalent_date_3',
-    'opv_date_1', 'opv_date_2', 'opv_date_3', 'ipv_date_1', 'ipv_date_2',
-    'pneumococcal_date_1', 'pneumococcal_date_2', 'pneumococcal_date_3', 'mmr_date_1', 'mmr_date_2',
-    'bcg_remarks', 'pentavalent_remarks', 'opv_remarks', 'ipv_remarks', 'pneumococcal_remarks', 'mmr_remarks'
+    'bcg_date_1', 'bcg_weight_1', 'bcg_remarks_1', 'bcg_date_2', 'bcg_weight_2', 'bcg_remarks_2',
+    'pentavalent_date_1', 'pentavalent_weight_1', 'pentavalent_remarks_1', 'pentavalent_date_2', 'pentavalent_weight_2', 'pentavalent_remarks_2', 'pentavalent_date_3', 'pentavalent_weight_3', 'pentavalent_remarks_3',
+    'opv_date_1', 'opv_weight_1', 'opv_remarks_1', 'opv_date_2', 'opv_weight_2', 'opv_remarks_2', 'opv_date_3', 'opv_weight_3', 'opv_remarks_3',
+    'ipv_date_1', 'ipv_weight_1', 'ipv_remarks_1', 'ipv_date_2', 'ipv_weight_2', 'ipv_remarks_2',
+    'pneumococcal_date_1', 'pneumococcal_weight_1', 'pneumococcal_remarks_1', 'pneumococcal_date_2', 'pneumococcal_weight_2', 'pneumococcal_remarks_2', 'pneumococcal_date_3', 'pneumococcal_weight_3', 'pneumococcal_remarks_3',
+    'mmr_date_1', 'mmr_weight_1', 'mmr_remarks_1', 'mmr_date_2', 'mmr_weight_2', 'mmr_remarks_2'
 );
 
 // Initialize an empty array to store SQL update parts
@@ -19,20 +21,21 @@ $updateParts = array();
 foreach ($inputNames as $inputName) {
     if (isset($_POST[$inputName]) && $_POST[$inputName] != '') {
         $inputValue = mysqli_real_escape_string($con, $_POST[$inputName]);
-        if (strpos($inputName, '_date_') !== false) { // Check if the field is a date
+        if (strpos($inputName, '_date_') !== false || strpos($inputName, '_weight_') !== false) {
+            // Check if the field is a date or weight
             $updateParts[] = "$inputName = '$inputValue'";
-        } else { // For text fields (remarks)
+        } else {
+            // For text fields (remarks)
             $updateParts[] = "$inputName = '$inputValue'";
         }
     } else {
-        // Set empty dates to NULL and skip empty remarks
-        if (strpos($inputName, '_date_') !== false) {
+        // Set empty dates and weights to NULL, and skip empty remarks
+        if (strpos($inputName, '_date_') !== false || strpos($inputName, '_weight_') !== false) {
             $updateParts[] = "$inputName = NULL";
         }
     }
 }
 
-// Construct the update query for the immunization table
 $updateQuery = "UPDATE immunization SET
     patient_id = '$patient_id',
     " . implode(', ', $updateParts) . "
@@ -44,7 +47,6 @@ if (!$results) {
     exit();
 } else {
     echo 'success';
-    // If needed, add a function to process immunization data here.
 }
 
 exit();
